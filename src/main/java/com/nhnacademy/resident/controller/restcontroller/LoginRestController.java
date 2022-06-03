31226ada7b4d49af3041c81a4e61d5d098b668c5
ComSun <computerhermit96@gmail.com>
2022-06-03 15:35:29 +0900

    docs: update README.md

---
+++ src/main/java/com/nhnacademy/resident/controller/restcontroller/LoginRestController.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.resident.controller.restcontroller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginRestController {
+}
